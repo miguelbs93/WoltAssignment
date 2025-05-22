ed3083a99110a038a9f03933c7f5c918b2a9aba0
Miguel Bou Sleiman <mbousleiman@hotmail.com>
2025-05-22 13:25:53 +0200

    Created Project

---
+++ WoltAssignment/WoltAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WoltAssignment
+//
+//  Created by Miguel Bou Sleiman on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WoltAssignment/WoltAssignment/WoltAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoltAssignmentApp.swift
+//  WoltAssignment
+//
+//  Created by Miguel Bou Sleiman on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WoltAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
