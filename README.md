0baa6124200b80c6ccdb800dad6e9926d61d56dc
Miguel Bou Sleiman <mbousleiman@hotmail.com>
2025-05-26 00:07:40 +0200

    Nit picks + read me file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # WoltAssignment
+
+This project is an iOS app demonstrating an implementation of a feature for discovering and favoriting nearby venues. 
+
+## Features
+
+- Fetches venues based on a rotating set of predefined locations
+- Displays a scrollable list of venues with support for:
+  - Loading indicator
+  - Animated transitions between location changes
+  - Favorites functionality with persistence (UserDefaults - since im only storing the venue's id)
+- Uses Swift Concurrency (`async/await`)
+- Implements MVVM architecture
+- Using SPM for modularization
+
--- WoltAssignment/Packages/Sources/VenuesList/VenuesList.swift
+++ WoltAssignment/Packages/Sources/VenuesList/VenuesList.swift
@@ -5,17 +5,31 @@ public struct VenuesList: View {
     
     public var body: some View {
         NavigationView {
-            VStack(spacing: 0) {
-                if viewModel.isLoading {
-                    HStack {
-                        Spacer()
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle())
-                            .padding(8)
-                        Spacer()
+            ZStack {
+                VStack(spacing: 0) {
+                    if viewModel.isLoading {
+                        HStack {
+                            Spacer()
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle())
+                                .padding(8)
+                            Spacer()
+                        }
+                        .background(Color(.systemGroupedBackground))
+                        .transition(.move(edge: .top).combined(with: .opacity))
                     }
-                    .background(Color(.systemGroupedBackground))
-                    .transition(.move(edge: .top).combined(with: .opacity))
+                    
+                    List(viewModel.venues) { item in
+                        VenuesRow(
+                            venue: item,
+                            isFavorite: viewModel.isFavorite(item),
+                            onFavoriteTapped: {
+                                viewModel.toggleFavorite(for: item)
+                            })
+                    }
+                    .refreshable {
+                        await viewModel.fetchVenues()
+                    }
                 }
                 
                 if let message = viewModel.errorMessage {
@@ -23,18 +37,6 @@ public struct VenuesList: View {
                         .foregroundColor(.red)
                         .padding()
                         .multilineTextAlignment(.center)
-                }
-                
-                List(viewModel.venues) { item in
-                    VenuesRow(
-                        venue: item,
-                        isFavorite: viewModel.isFavorite(item),
-                        onFavoriteTapped: {
-                            viewModel.toggleFavorite(for: item)
-                    })
-                }
-                .refreshable {
-                    await viewModel.fetchVenues()
                 }
             }
             .background(Color(.systemGroupedBackground))
